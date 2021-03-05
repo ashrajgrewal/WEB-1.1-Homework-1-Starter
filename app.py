2dfd4d5d2ccc65cd42a408a4cb98b4272429084f
Ashraj Grewal <ashraj.grewal10@gmail.com>
2021-03-05 13:33:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -22,3 +22,34 @@ def favorite_dessert(users_dessert):
 def generate_madlib(adjective, noun):
     return f"Today I went to the living room and saw a(n) {adjective} {noun} sitting on my couch!"
 
+@app.route('/multiply/<num_1>/<num_2>')
+def multiply(num_1, num_2):
+    if num_1.isdigit() and num_2.isdigit() == True: 
+        result: int = int(num_1) * int(num_2)
+        return f"The result is {result}"
+    else: 
+        return 'Invalid inputs. Please try again by entering 2 numbers!'
+
+@app.route('/sayntimes/<word>/<n>')
+def sayNTimes(word, n):
+   n = int(n)
+   repeatedWord = ''
+   for i in range(n):
+      repeatedWord += word
+   return repeatedWord
+
+@app.route('/dicegame')
+def roll_dice():
+    sides = [1, 2, 3, 4, 5, 6]
+    result = choice(sides)
+    if result == 6:
+        return "You Rolled A 6, You Won!" 
+    else: 
+        return f"You Rolled a {result}, You Lose!" 
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route('/profile/<users_name>')
+def profile(users_name):
+    return "Hello " + users_name
